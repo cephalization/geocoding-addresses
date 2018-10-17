a4dee95c47887ebd3e33abaca8508f0528e87f5b
Anthony Powell <apowell@arbor.net>
2018-10-17 16:34:12 -0400

    Parse addresses from file
    - TODO: attempt to geoencode each string, only save those who are valid
        to try and prevent out-of-memory errors from saving every address

--- addressParser/addressParser.js
+++ addressParser/addressParser.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const targz = require('targz');
+const reader = require('line-by-line');
 
 const ADDRESS_PARSE_RULES = [
   { rule: 'House Number', limit: 30, delim: ' ' },
   { rule: 'Street Direction Prefix', limit: 2, delim: ' ' },
   { rule: 'Street Name', limit: 40, delim: ' ' },
   { rule: 'Street Suffix', limit: 4, delim: ' ' },
-  { rule: 'Street Direction Suffix', limit: 4, delim: ', ' },
+  { rule: 'Street Direction Suffix', limit: 2, delim: ', ' },
   { rule: 'Unit Descriptor', limit: 10, delim: ' ' },
   { rule: 'Unit Number', limit: 6, delim: ', ' },
   { rule: 'City', limit: 30, delim: ', ' },
@@ -36,17 +37,52 @@ const decompressFile = (src, dest, callback) => {
 };
 
 /**
+ * Given a line of address content, parse based on ADDRESS_PARSE_RULES
+ *
+ * @param {string} line address line from addresses.txt
+ *
+ * @return {string} formatted address
+ */
+const processAddressLine = (line) => {
+  let address = '';
+  let readCharacters = 0;
+
+  ADDRESS_PARSE_RULES.forEach((rule) => {
+    const segment = line
+      .slice(readCharacters, readCharacters + rule.limit)
+      .trim();
+    readCharacters += rule.limit;
+
+    address = `${address}${segment}${segment.length ? rule.delim : ''}`;
+  });
+
+  return address;
+};
+
+/**
  * Read address text file
  *
  * @param {string} srcDir directory containing addresses.txt file containing address information
  */
 const readAddressesFile = (srcDir) => {
-  console.log('reading not implemented');
-  // For each rule, read that many characters from the text file
+  console.log('Reading addresses...');
+  return new Promise((resolve) => {
+    const lines = new reader(ADDRESS_FILE, { encoding: 'utf8', skipEmptyLines: true });
+    const addresses = [];
 
-  // Add to the current address as a string, delimited by the rule
+    // Process each line based on the rules, add it to array of addresses
+    lines.on('line', line => addresses.push(processAddressLine(line)));
 
-  return [];
+    lines.on('error', (err) => {
+      console.log('Error parsing addresses file...', err);
+      resolve(addresses);
+    });
+
+    lines.on('end', () => {
+      console.log(addresses.length, 'addresses parsed');
+      resolve(addresses);
+    });
+  });
 };
 
 const geocodeAddresses = async (addresses) => {
@@ -81,16 +117,18 @@ const getGeocodedAddresses = async (src) => {
 
   if (!fs.existsSync(ADDRESS_FILE)) {
     console.log('File does not exist, uncompressing archive...');
-    addresses = await decompressFile(src, ADDRESS_FILE, parseAddresses);
+    addresses = await decompressFile(src, './', parseAddresses);
   } else {
     console.log('File exists, re-parsing archive');
     addresses = await parseAddresses();
   }
 
+  console.log('addresses parsed');
+
   return addresses;
 };
 
-// getGeocodedAddresses('./addresses.tar.gz');
+getGeocodedAddresses('./addresses.tar.gz');
 
 module.exports = {
   getGeocodedAddresses,
